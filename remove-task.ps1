2400177ed87f6b07b16b88b5ad477f216a703acd
Andrew Randall <arandall@cardinalsolutions.com>
2018-09-12 09:20:06 -0500

    Remove task by name

---
+++ remove-task.ps1
@@ -0,0 +1,17 @@
+param($name)
+
+if (-not(Test-Path .dat)) {
+    return
+}
+
+Move-Item .dat .datbak
+
+foreach ($line in Get-Content .datbak) {
+    if ($line.Substring(2) -ne $name) {
+        Add-Content .dat $line
+    }
+}
+
+Remove-Item .datbak
+
+.\list-tasks.ps1
