f666895fdabb5cacfc660d5937f7051ff1d9213b
Andrew Randall <arandall@cardinalsolutions.com>
2018-09-12 09:07:31 -0500

    Prevent adding a duplicate task

--- add-task.ps1
+++ add-task.ps1
@@ -4,4 +4,15 @@ if (-not (Test-Path .dat)) {
     .\init.ps1
 }
 
+foreach ($line in Get-Content .dat) {
+    if ($line[0] -eq "-") {
+        $otherTaskName = $line.Substring(2)
+        if ($name -eq $otherTaskName) {
+            Write-Host "Task already exists"
+            return
+        }
+    }
+}
+
 Add-Content .dat "- $($name)"
+.\list-tasks.ps1
